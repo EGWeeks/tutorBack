8826a2d24f7c1ecae39600858759b0b78ef2facb
Ethan Weeks <ethanweeks@ymail.com>
2016-03-05 21:29:55 -0700

    inital commit/stripped to the bone

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var express = require('express');
+var router  = express.Router();
+
+var getAll  = require('./users/getAll'),
+		postNew = require('./users/postNew'),
+		getOne  = require('./users/getOne'),
+		putOne  = require('./users/putOne'),
+		delOne  = require('./users/delOne'),
+		signIn  = require('./users/signIn'),
+		signOut = require('./users/signOut');
+
+	router.get('/', getAll); // <------------ GET ‘/’ - shows all resources
+	router.post('/new', postNew); // <------- POST ‘/new’ - creates individual
+	router.get('/:id', getOne); // <--------- GET ‘/:id’ - shows individual resource
+	router.put('/:id', putOne); // <--------- PUT ‘/:id’ - updates individual resource
+	router.delete('/:id', delOne); // <--- DELETE ‘/:id’ - removes resource
+	router.post('/signin', signIn); // <----- POST '/signin' - authenticates user on sign in
+	router.post('/signout', signOut); // <----- POST '/signout' - signs out current user
+
+module.exports = router;
---
+++ routes/users/getAll.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var Table = require('../../db/knex'),
+    Users = Table('users');
+
+// GET ‘/’ - shows all resources
+function getAllUsersHandler(req, res) {
+  Users()
+    .select('first_name', 'last_name', 'email')
+    .then(function(usersData) {
+      res.send( {
+        title: 'Users',
+        users: usersData,
+        session: req.session
+      });
+    });
+}
+
+module.exports = getAllUsersHandler;
