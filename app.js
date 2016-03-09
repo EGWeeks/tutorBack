91b335e9cb25c96a8a871fbf7bd12c4bf20c683c
Ethan Weeks <ethanweeks@ymail.com>
2016-03-09 10:35:00 -0700

    added passpart/ basic app setup/ no route set up

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require('express');
 // var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
 
 var users = require('./routes/users');
 
@@ -14,7 +16,8 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/users', users);
 
 // catch 404 and forward to error handler
