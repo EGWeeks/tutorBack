28c01d47f5bd5c8bf37f5b1e60764af943bff638
Ethan Weeks <ethanweeks@ymail.com>
2016-03-09 19:26:57 -0700

    removed passport module/ postponed

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var express = require('express');
 // var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var passport = require('passport');
-var Strategy = require('passport-facebook').Strategy;
 
 var users = require('./routes/users');
 
@@ -16,8 +14,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+
 app.use('/users', users);
 
 // catch 404 and forward to error handler
