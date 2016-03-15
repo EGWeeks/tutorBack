62f44cc28de8c426550f5f619575dae1e88d6eb1
Ethan Weeks <ethanweeks@ymail.com>
2016-03-14 20:32:26 -0600

    rolled back users & update/created posts & relation table

--- migrations/20160305214823_users.js
+++ migrations/20160305214823_users.js
@@ -1,15 +1,38 @@
 'use strict';
 
-exports.up = function(knex) {
-  return knex.schema.createTable('users', function(table){
-    table.increments(); // id --> SERIAL PRIMARY KEY
-    table.string('first_name', 120); // first_name --> VARCHAR(120)
-    table.string('last_name', 120); // last_name --> VARCHAR(120)
-    table.string('email').unique().notNullable(); // email --> VARCHAR(255) Unique Not Null
-    table.string('password'); // password --> VARCHAR(255)
-  });
+exports.up = function(knex, Promise) {
+  return Promise.all([
+	  knex.schema.createTable('users', function(table){
+	    table.increments(); // id --> SERIAL PRIMARY KEY
+	    table.string('first_name', 120); // first_name --> VARCHAR(120)
+	    table.string('last_name', 120); // last_name --> VARCHAR(120)
+	    table.string('email').unique().notNullable(); // email --> VARCHAR(255) Unique Not Null
+	    table.text('bio'); // bio --> text
+	    table.string('location'); // location --> VARCHAR(255)
+	    table.string('password'); // password --> VARCHAR(255)
+	  }),
+
+	  knex.schema.createTable('user_post', function(table){
+	  	table.increments(); // id --> SERIAL PRIMARY KEY
+	  	table.integer('user_id'); // user id --> INTEGER
+	  	table.integer('post_id'); //post id --> INTEGER
+	  }),
+
+	  knex.schema.createTable('posts', function(table) {
+	  	table.increments(); // id --> SERIAL PRIMARY KEY
+	  	table.string('subject'); // subject --> VARCHAR(255)
+	  	table.string('type'); // type --> VARCHAR(255) instuctor or student
+	  	table.integer('rate'); // rate --> INTEGER hourly rate
+	  	table.string('avail'); // avaiblable --> VARCHAR(255)
+	  	table.string('desc'); // description --> VARCHAR(255)
+	  })
+	]);
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('users');
+exports.down = function(knex, Promise) {
+  return Promise.all([
+  	knex.schema.dropTable('users'),
+  	knex.schema.dropTable('user_post'),
+  	knex.schema.dropTable('posts')
+  ]);
 };
