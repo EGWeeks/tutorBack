75459b295bb24ceef8d56f0e946022291734eb24
Ethan Weeks <ethanweeks@ymail.com>
2016-03-16 10:58:05 -0600

    added get users posts

--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,14 @@
 var express = require('express');
 var router  = express.Router();
 
-var	postNew	= require('./posts/postNew');
+var	postNew	= require('./posts/postNew'),
+ getMyPosts = require('./posts/getMyPosts');
 		// getPost = require('./posts/getPost'),
 		// putPost = require('./posts/putPost'),
 		// delPost = require('./posts/delPost');
 
 	router.post('/', postNew);	// <-- POST '/' - creates new post
+	router.get('/my/:id', getMyPosts);
 	// router.get('/:id', getPost); // <-- GET ‘/:id’ - shows individual post
 	// router.put('/:id', putPost); // <-- PUT ‘/:id’ - updates individual post
 	// router.delete('/:id', delPost); // <-- DELETE ‘/:id’ - removes post
