8a5e7a792d95b81c133a8eae4f6a305bf4dfd897
Ethan Weeks <ethanweeks@ymail.com>
2016-03-16 09:52:25 -0600

    create new post route/100

--- migrations/20160305214823_users.js
+++ migrations/20160305214823_users.js
@@ -13,12 +13,6 @@ exports.up = function(knex, Promise) {
 	    table.text('img'); // image --> TEXT
 	  }),
 
-	  knex.schema.createTable('user_post', function(table){
-	  	table.increments(); // id --> SERIAL PRIMARY KEY
-	  	table.integer('user_id'); // user id --> INTEGER
-	  	table.integer('post_id'); //post id --> INTEGER
-	  }),
-
 	  knex.schema.createTable('posts', function(table) {
 	  	table.increments(); // id --> SERIAL PRIMARY KEY
 	  	table.string('subject'); // subject --> VARCHAR(255)
@@ -26,6 +20,7 @@ exports.up = function(knex, Promise) {
 	  	table.integer('rate'); // rate --> INTEGER hourly rate
 	  	table.string('avail'); // avaiblable --> VARCHAR(255)
 	  	table.string('desc'); // description --> VARCHAR(255)
+	  	table.integer('user_id'); // user ID --> INTEGER
 	  })
 	]);
 };
@@ -33,7 +28,6 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   return Promise.all([
   	knex.schema.dropTable('users'),
-  	knex.schema.dropTable('user_post'),
   	knex.schema.dropTable('posts')
   ]);
 };
