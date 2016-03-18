a4eb53fcb49663a9468653cecf9a58a619e8ce91
Ethan Weeks <ethanweeks@ymail.com>
2016-03-18 11:29:27 -0600

    get post by user id and post id match

--- routes/posts.js
+++ routes/posts.js
@@ -4,14 +4,14 @@ var express = require('express');
 var router  = express.Router();
 
 var	postNew	= require('./posts/postNew'),
- getMyPosts = require('./posts/getMyPosts');
-		// getPost = require('./posts/getPost'),
+ getMyPosts = require('./posts/getMyPosts'),
+		getPost = require('./posts/getPost');
 		// putPost = require('./posts/putPost'),
 		// delPost = require('./posts/delPost');
 
 	router.post('/', postNew);	// <-- POST '/' - creates new post
 	router.get('/my/:id', getMyPosts);
-	// router.get('/:id', getPost); // <-- GET ‘/:id’ - shows individual post
+	router.get('edit/:id/:post', getPost); // <-- GET ‘/:id’ - shows individual post
 	// router.put('/:id', putPost); // <-- PUT ‘/:id’ - updates individual post
 	// router.delete('/:id', delPost); // <-- DELETE ‘/:id’ - removes post
 
