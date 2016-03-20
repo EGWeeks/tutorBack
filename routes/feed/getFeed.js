ea0fdf5469dfb96ced1f202242164a6536a8524d
Ethan Weeks <ethanweeks@ymail.com>
2016-03-20 12:31:27 -0600

    limiting query to return 15 results

--- routes/feed/getFeed.js
+++ routes/feed/getFeed.js
@@ -5,9 +5,10 @@ var Table = require('../../db/knex.js'),
 
 // GET ‘/:id’ - shows individual resource
 function getOneUserHandler(req, res) {
-
+  //Query posts and users limited to 15 rows
   Posts()
   .join('users', 'posts.user_id', '=', 'users.id')
+  .liit(15)
   .then(function(postData) {
     res.json({
       post: postData
