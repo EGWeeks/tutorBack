d518da715896b6022acec8ebee23eefaff695930
Ethan Weeks <ethanweeks@ymail.com>
2016-03-27 13:35:14 -0600

    testing search route; ;

--- routes/posts.js
+++ routes/posts.js
@@ -6,11 +6,13 @@ var router  = express.Router();
 var  postNew = require('./posts/postNew'),
  	getMyPosts = require('./posts/getMyPosts'),
 		 getPost = require('./posts/getPost'),
-		 putPost = require('./posts/putPost');
+		 putPost = require('./posts/putPost'),
+	 getSearch = require('./posts/getPostingsSearch');
 
 	router.post('/', postNew);	// <-- POST '/' - creates new post
 	router.get('/my/:id', getMyPosts); // <-- GET '/my/:id' - shows all that specific users post
 	router.get('/my/:id/:post', getPost); // <-- GET ‘/:id/:post’ - shows individual post
 	router.put('/my/:id/:post', putPost); // <-- PUT ‘/:id’ - updates individual post
+	router.get('/search/:sport/:type', getSearch);
 
 module.exports = router;
