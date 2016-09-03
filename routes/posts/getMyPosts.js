1d09fdd1b0a643ee7b116b4cea3de2f6de6a0400
Ethan Weeks <ethanweeks@ymail.com>
2016-09-02 23:15:50 -0600

    add logs development hitting route but not returning local data + maybe still connected to aws

--- routes/posts/getMyPosts.js
+++ routes/posts/getMyPosts.js
@@ -3,9 +3,11 @@
 var Table = require('../../db/knex.js'),
   Posts = Table('posts');
 
+// https://obscure-earth-64971.herokuapp.com
+
 // GET ‘my/:id’ - shows user posts 
 function getUserPostsHandler(req, res) {
-
+  console.log("stuff...............stufff");
   var currentDate = new Date();  
 
   Posts()
@@ -18,6 +20,7 @@ function getUserPostsHandler(req, res) {
       data.forEach(function(post){
         // Check all rows with status = Active and if todays date is greater than expiration date
         if(post.status === 'Active' && currentDate > post.expiration) {
+          console.log(post.expiration + ' made into the post.status if');
           // If expired Update that row status = Expired
           Posts()
             .where({
